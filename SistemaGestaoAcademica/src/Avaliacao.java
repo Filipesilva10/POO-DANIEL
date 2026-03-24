e8fd64e7397c27db1e22483db3fe5d84f5931350
Filipe Silva Rodrigues <filipesirodrigues@gmail.com>
2026-03-23 21:57:59 -0300

    att

---
+++ SistemaGestaoAcademica/src/Avaliacao.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Avaliacao {
+    private int id;
+    private String nome;
+    private ArrayList<Questao> Questoes;
+
+    // Metodo de Composição: Cria o objeto questão internamente
+    public void adicionarQuestao(int num, String texto, float peso){
+        Questao aux = new Questao(num, texto, peso);
+        this.Questoes.add(aux);
+        System.out.println("Questão adicionada com sucesso.");
+    }
+
+    public Avaliacao() {
+        this.Questoes = new ArrayList<>();
+    }
+
+    public Avaliacao(int id, String nome){
+        this.id = id;
+        this.nome = nome;
+        this.Questoes = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "\nID = " + id +
+                ", NOME = " + nome +
+                "\nQUESTÕES = " + Questoes;
+    }
+}
